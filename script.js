7b929bd68f0d37c5b4536d8873cf62c5a6ffca41
saurabh singh <86177535+saurabhsngh5484@users.noreply.github.com>
2023-04-15 10:29:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	let fname = document.getElementById("fname").value;
+	let lname = document.getElementById("lname").value;
 
+	alert(`${fname} ${lname}`);
 }
